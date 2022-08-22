bbe6d7fa7efc5afc81aae5b3674f8c27520faf64
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-08-22 08:15:34 +0430

    [deployment auto commit]

---
+++ website/out/_next/static/0cMs4N-dp9C7qai-FIk3J/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/886-b01523d78db12afe.js","static/chunks/pages/index-c9e34c637528c02a.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- website/pages/index.js
+++ website/pages/index.js
@@ -182,7 +182,7 @@ export default function Main(){
 
                         {/* custom style */}
                         <h3 className='sectionName' id='custom-style'><a href='#custom-style'># Custom Style</a></h3>
-                        <p className='sectionDescription'>The Awesome Toast's html markup is equivalent to below:</p>
+                        <p className='sectionDescription'>The Awesome Toast&apos;s html markup is equivalent to below:</p>
                         <div className='codeWrapper'>
                             <button className='codeSection'>
                                 <p>
